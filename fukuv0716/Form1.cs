82848e06d42150e1dc0d6c3aaf5d8ec18e40c3c3
Fukata <satoshidat@gmail.com>
2020-07-16 10:14:05 +0900

    完成

--- fukuv0716/Form1.cs
+++ fukuv0716/Form1.cs
@@ -16,5 +16,31 @@ namespace fukuv0716
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top += int.Parse(textBox1.Text) -10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
